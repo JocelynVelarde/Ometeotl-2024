38ff94200f788df18622626ed5bcd8224ef2042a
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-10-05 17:45:33 -0600

    Added pymongo connection

--- Ometeotl_2024/pages/fire.py
+++ Ometeotl_2024/pages/fire.py
@@ -2,9 +2,40 @@
 
 from ..templates import template
 from Ometeotl_2024.components.map import fire_map
+from pymongo.server_api import ServerApi
+from pymongo import MongoClient
+from ..backend.json_encoder import JSONEncoder
 
 import reflex as rx
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+client = MongoClient(uri=os.getenv("MONGO_URI"), server_api=ServerApi('1'))
+
+def insert_data(data :str, database: str, collection: str) -> str:
+    try:
+        database =  client.get_database(database)
+        collection = database.get_collection(collection)
+        
+        result = collection.insert_one(data)
+        
+        print(f'Data inserted successfully with id: {result.inserted_id}')
+    except Exception as e:
+        print("Error inserting data: ", e)
+
+def get_one_data(query: str, database: str, collection: str) -> str:
+    try:
+        database =  client.get_database(database)
+        collection = database.get_collection(collection)
+        
+        data = collection.find_one(query)
+        
+        return JSONEncoder().encode(data)
+    except Exception as e:
+        print("Error getting data: ", e)
+        return {"error": f'Error getting data {e}'}
 
 @template(route="/fire", title="Fire")
 def fire() -> rx.Component:

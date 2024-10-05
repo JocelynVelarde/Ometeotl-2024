d974ac8667d3d3368c4746fa8c4d3924c2d3acb3
JocelynVelarde <a01285780@itesm.mx>
2024-10-05 09:22:26 -0600

    first commit

---
+++ Ometeotl_2024/components/card.py
@@ -0,0 +1,12 @@
+import reflex as rx
+from Ometeotl_2024 import styles
+
+
+def card(*children, **props):
+    return rx.card(
+        *children,
+        box_shadow=styles.box_shadow_style,
+        size="3",
+        width="100%",
+        **props,
+    )

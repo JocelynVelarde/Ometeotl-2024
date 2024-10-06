f53e9416bb6d170f39371c343ed5fec3094ece40
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-10-06 00:41:29 -0600

    Created location by ip

--- api/location_fetcher.py
+++ api/location_fetcher.py
@@ -1,6 +1,8 @@
+from geopy.geocoders import Photon
+
 import requests
 
-def get_location() -> tuple:
+def get_latlon() -> tuple:
     response = requests.get('https://ipinfo.io')
 
     data = response.json()
@@ -11,3 +13,12 @@ def get_location() -> tuple:
     else:
         print("Could not retrieve location data.")
         return '', ''
+
+def get_location_by_ip():
+    lat, lon = get_latlon()
+
+    # Use Geopy to convert latitude and longitude to a human-readable address
+    geolocator = Photon(user_agent="measurements")
+    location = geolocator.reverse(f"{lat}, {lon}", language="en")
+
+    return location.address

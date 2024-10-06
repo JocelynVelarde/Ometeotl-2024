fcd03824de72fee8f7fd14fc5d253ab3288c9510
Alejandra Coeto <a01285221@tec.mx>
2024-10-06 10:19:16 -0600

    rm comments

--- pages/_🌾_Crop.py
+++ pages/_🌾_Crop.py
@@ -21,43 +21,25 @@ st.divider()
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 # Camera input
-# uploaded_file = st.camera_input("Capture a picture")
-
-# Initialize session state for camera enabled
 if 'camera_enabled' not in st.session_state:
     st.session_state.camera_enabled = False
 
-# Button to enable camera
 if st.session_state.camera_enabled == False:
     if st.button("Enable Camera"):
         st.session_state.camera_enabled = True
 
-# Display camera input only if the camera is enabled
 if st.session_state.camera_enabled:
     picture = st.camera_input("Capture a picture")
 
-
-    # If a picture is taken, display it
     if picture is not None:
         st.image(picture, caption='Captured Image', use_column_width=True)
 
-    # Button to cancel/disable camera input
     if st.button("Cancel Camera"):
         st.session_state.camera_enabled = False
-# Inform the user if the camera is disabled
+
 if not st.session_state.camera_enabled:
     st.write("Camera is not enabled.")
 
-# # Display message if camera is disabled
-# if not st.session_state.camera_enabled:
-#     st.write("Camera is not enabled.")
-
-# if picture is not None:
-#     # To read file as bytes
-#     st.image(picture, caption='Captured Image', use_column_width=True)
-# st.write(st.session_state.phone_number)
-
-# Display the file details after uploading
 if uploaded_file is not None:
     st.write(f"Uploaded file: {uploaded_file.name}")
 
@@ -73,5 +55,3 @@ if uploaded_file is not None:
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     st.image(image_rgb, caption="Results", use_column_width=True)
     st.success("Analysis complete")
-
-# Display the image

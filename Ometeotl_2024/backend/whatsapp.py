b7719d9292aa40601344221632a89d886f81f11c
Alejandra Coeto <a01285221@tec.mx>
2024-10-05 22:11:06 -0600

    rm

--- Ometeotl_2024/backend/whatsapp.py
+++ Ometeotl_2024/backend/whatsapp.py
@@ -3,7 +3,7 @@ import json
 
 # Replace these variables with your own values
 api_url = "https:///v1/messages"  # Update with your API URL
-access_token = "EAAUvhit0i70BOZBQT7I2vf9YWnXJxS9ZA4vsTZAyZAM2auCXnOcZAdLn5FQ9hZCs1xUlwnr68cpMJCVqgbuOlFPWu2EaqWrJk0o80DpSJPcTVtf4qMvkXQymxECze3dI5GoWhdhmTFZBZA7kAQzJn4I2ipAxjcQh6nS0ULlIt1Vnid1lMmsfUaKN8e9iweg4DLR5Rd2WPvGZCyzEVGFE4QPoTgQKgjkYZD"  # Your access token
+access_token = ""  # Your access token
 to_phone_number = "<RECIPIENT_PHONE_NUMBER>"  # Recipient's phone number with country code
 message = "Hello from Python using WhatsApp Business API!"
 
